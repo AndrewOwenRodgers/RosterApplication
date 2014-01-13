c85a41c84b6b7e2816acc9fd586bda69d394885f
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-01-13 11:30:55 -0800

    This is my first commit

--- FirstDayDemo/ViewController.h
+++ FirstDayDemo/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+<UITableViewDataSource, UITableViewDelegate>
+
 @end
--- FirstDayDemo/ViewController.m
+++ FirstDayDemo/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property (strong, nonatomic) NSArray *myStudentsArray;
 
 @end
 
@@ -17,8 +19,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *student1 = [NSString stringWithFormat:@"Brad"];
+    NSString *student2 = [NSString stringWithFormat:@"Clem"];
+    NSString *student3 = [NSString stringWithFormat:@"Billy Bob"];
+    NSString *student4 = @"John";
+    
+    self.myStudentsArray = [NSArray arrayWithObjects:student1, student2, student3, student4, nil];
+    
+    self.myTableView.delegate = self;
+    self.myTableView.dataSource = self;
 }
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.myStudentsArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell" forIndexPath: indexPath];
+    cell.textLabel.text = [self.myStudentsArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
