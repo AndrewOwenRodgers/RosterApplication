be35276ec95c840a0642800435484cfa94b1aeb6
AndrewOwenRodgers <owenrhyfelwr@gmail.com>
2014-01-13 09:58:35 -0800

    Initial Commit

---
+++ FirstDayDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstDayDemo
+//
+//  Created by Andrew Rodgers on 1/13/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
